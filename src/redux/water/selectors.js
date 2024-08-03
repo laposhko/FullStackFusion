6ed2230ea343f2fc73b6180d26f02d20efa6bace
laposhko <92787511+laposhko@users.noreply.github.com>
2024-08-03 21:09:22 +0300

    Update selectors.js

--- src/redux/water/selectors.js
+++ src/redux/water/selectors.js
@@ -4,5 +4,5 @@ export const selectWaterCards = state => state.water.cards;
 export const selectWaterIsLoading = state => state.water.isLoading;
 export const selectWaterIsError = state => state.water.isError;
 
-export const selectMonth = (state) => state.water.selectedMonth;
-export const selectDate = (state) => state.water.selectedDate;
+// export const selectMonth = (state) => state.water.selectedMonth;
+// export const selectDate = (state) => state.water.selectedDate;
