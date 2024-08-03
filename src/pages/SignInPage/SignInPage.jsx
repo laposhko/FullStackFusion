d4940667bf95468a331e5bb0662fd0e5d49cd508
MariaRadchuk <mradchuk_15@ukr.net>
2024-08-03 18:17:23 +0300

    signin

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,3 +1,29 @@
-export default function SignInPage() {
-  return <h1>Sign in</h1>;
-}
+import React, { useState, useEffect } from 'react';
+import SignInForm from 'src/components/SignInForm/SignInForm.jsx';
+import AdvantagesSection from 'src/components/AdvantagesSection/AdvantagesSection';
+import css from './SignInPage.module.css';
+// import Container from 'src/components/Container/Container'; //нету такого файла, но по логике должен быть
+
+const SignInPage = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <Container>
+      <div className={css.signInContainer}>
+        <SignInForm />
+        {!isMobile && <AdvantagesSection />}
+      </div>
+    </Container>
+  );
+};
+
+export default SignInPage;
