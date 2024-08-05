25106ca4e5aa707b84e4934512deecfebd894ee0
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-08-05 18:01:53 +0300

    testModal

---
+++ src/components/TestModals.jsx
@@ -0,0 +1,32 @@
+import { useModalContext } from '../context/useModalContext.jsx';
+// import { icons as sprite } from '../../../../shared/icons/index.js';
+import sprite from './../../img/icons/sprite.svg';
+
+
+import DeleteWaterModal from '../../../Modals/DeleteWaterModal/DeleteWaterModal.jsx';
+import css from './TestModals.module.css';
+
+function WaterItem() {
+
+
+    return (
+   
+        <div className={css.btns}>
+          
+          <button
+            className={css.btn}
+            onClick={() => {
+              openModal(<DeleteWaterModal onDelete={id} />);
+            }}
+          >
+            <svg className="icon icon-trash">
+              <use xlinkHref={`${sprite}#icon-trash`}></use>
+            </svg>
+          </button>
+        </div>
+    )
+    }
+
+    export default WaterItem;
+  
+   
