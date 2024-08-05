f849eac2475fbb689beefaf57338cef9be508843
laposhko <laposhko0@gmail.com>
2024-08-06 00:07:10 +0300

    trying to fix deploy err

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,5 +1,5 @@
 import UserPanel from "../../components/UserPanel/UserPanel";
-import css from "./TrackerPage.module.css";
+// import css from "./TrackerPage.module.css";
 
 export default function TrackerPage() {
   return (
