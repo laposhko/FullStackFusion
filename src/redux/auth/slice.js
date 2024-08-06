c0e45cabf5ba5ce9203cd408ffdf6f21b440a9d8
laposhko <laposhko0@gmail.com>
2024-08-06 23:34:13 +0300

    fixed conflicts

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,97 +1,98 @@
-
 import { createSlice } from "@reduxjs/toolkit";
-import { googleAuthorization, refresh, signIn, signOut, signUp } from "./operations";
+import { refresh, signIn, signOut, signUp } from "./operations";
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState: {
-        user: {
-            name: null,
-            email: null,
-            gender: null,
-            weight: null,
-            dailyActivity: null,
-            dailyWaterNorm: null,
-            avatar: null,
-            id: null,
-            createdAt: null,
-            updatedAt: null,
-        },
-        token: null,
-        isLoading: false,
-        isLoggedIn: false,
-        isRefreshing: false,
-        isError: false, 
+  name: "auth",
+  initialState: {
+    user: {
+      name: null,
+      email: null,
+      gender: null,
+      weight: null,
+      dailyActivity: null,
+      dailyWaterNorm: null,
+      avatar: null,
+      id: null,
+      createdAt: null,
+      updatedAt: null,
     },
-    extraReducers: builder => builder.addCase(signUp.pending, (state) => {
+    token: null,
+    isLoading: false,
+    isLoggedIn: false,
+    isRefreshing: false,
+    isError: false,
+  },
+  extraReducers: (builder) =>
+    builder
+      .addCase(signUp.pending, (state) => {
         state.isError = false;
         state.isLoading = true;
-    })
-    .addCase(signUp.fulfilled, (state, action) => {
+      })
+      .addCase(signUp.fulfilled, (state, action) => {
         state.isError = false;
         state.isLoading = false;
         state.isLoggedIn = true;
         state.token = action.payload.token;
         state.user = action.payload.user;
-    })
-    .addCase(signUp.rejected, (state) => {
+      })
+      .addCase(signUp.rejected, (state) => {
         state.isLoading = false;
         state.isError = true;
-    })
-    .addCase(signIn.pending, (state) => {
+      })
+      .addCase(signIn.pending, (state) => {
         state.isError = false;
         state.isLoading = true;
-    })
-    .addCase(signIn.fulfilled, (state, action) => {
+      })
+      .addCase(signIn.fulfilled, (state, action) => {
         state.isError = false;
         state.isLoading = false;
         state.isLoggedIn = true;
         state.token = action.payload.token;
         state.user = action.payload.user;
-    })
-    .addCase(signIn.rejected, (state) => {
+      })
+      .addCase(signIn.rejected, (state) => {
         state.isLoading = false;
         state.isError = true;
-    })
-    .addCase(signOut.pending, (state) => {
+      })
+      .addCase(signOut.pending, (state) => {
         state.isError = false;
         state.isLoading = true;
-    })
-    .addCase(signOut.fulfilled, (state) => {
-       state.user = {
-        name: null,
-        email: null,
-        gender: null,
-        weight: null,
-        dailyActivity: null,
-        dailyWaterNorm: null,
-        avatar: null,
-        id: null,
-        createdAt: null,
-        updatedAt: null,
-    };
-    state.googleAuthLink = null;
-    state.token = null;
-    state.isLoading = false;
-    state.isError = false;
-    state.isLoggedIn = false;
-    })
-    .addCase(refresh.pending, (state) => {
+      })
+      .addCase(signOut.fulfilled, (state) => {
+        state.user = {
+          name: null,
+          email: null,
+          gender: null,
+          weight: null,
+          dailyActivity: null,
+          dailyWaterNorm: null,
+          avatar: null,
+          id: null,
+          createdAt: null,
+          updatedAt: null,
+        };
+        state.googleAuthLink = null;
+        state.token = null;
+        state.isLoading = false;
+        state.isError = false;
+        state.isLoggedIn = false;
+      })
+      .addCase(refresh.pending, (state) => {
         state.isRefreshing = true;
         state.isError = false;
         state.isLoggedIn = false;
-    })
-    .addCase(refresh.fulfilled, (state, action) => {
+      })
+      .addCase(refresh.fulfilled, (state, action) => {
         state.isRefreshing = false;
         state.isError = false;
         state.isLoggedIn = true;
         state.token = action.payload;
-    })
-    .addCase(refresh.rejected, (state) => {
+      })
+      .addCase(refresh.rejected, (state) => {
         state.isRefreshing = false;
         state.isLoggedIn = false;
         state.isError = true;
-    })
+      }),
 });
 
 const authReducer = authSlice.reducer;
