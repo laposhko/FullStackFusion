08f596d0b85f3f41af0a8034975d0fec3a9ec75f
Halyna Savchenko <galasavchenko1@gmail.com>
2024-08-07 15:28:11 +0200

    add clsx, context for addWaterBtn

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -1,16 +1,26 @@
-import css from './AddWaterBtn.module.css';
-import SvgIcon from '../../img/icons/sprite'
+import clsx from 'clsx';
+import style from './AddWaterBtn.module.css';
+import SvgIcon from '../../img/icons/sprite';
+import { useModalContext } from '../../context/useModalContext'
+import UserSettingsModal from '../UserSettingsModal/UserSettingsModal'
 
-export default function AddWaterBtn() {
+export default function AddWaterBtn({className}) {
+
+    const {openModal} = useModalContext()
     return(
         <div>
-            <button className={css.btn} type='button' onClick={()=> console.log("Click Button")}>
+            <button className={clsx(style.btn, className)} type='button' onClick={() => {
+              openModal(<UserSettingsModal></UserSettingsModal>);
+            }}>
+                <div className={style.iconWrapper}>
                 <SvgIcon
-                    className={css.icon}
+                    className={style.icon}
                     iconName="icon-close"
                     width={14}
                     height={14}    
                 /> 
+                </div>
+                
                 Add water
             </button>
         </div>
--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -3,6 +3,7 @@ import WaterDailyNorma from '../WaterDailyNorma/WaterDailyNorma';
 import WaterProgressBar from '../WaterProgressBar/WaterProgressBar';
 import AddWaterBtn from '../AddWaterBtn/AddWaterBtn';
 import css from './WaterMainInfo.module.css';
+import style from '../AddWaterBtn/AddWaterBtn.module.css';
 import bottle1d from '../../img/TrackerPage/bottle-for-water-desktop.png';
 import bottle2d from '../../img/TrackerPage/bottle-for-water-desktop@2x.png';
 import bottle1m from '../../img/TrackerPage/bottle-for-water-mobile.png';
@@ -16,7 +17,7 @@ export default function WaterMainInfo() {
             <Logo></Logo>
             <WaterDailyNorma></WaterDailyNorma>
             <WaterProgressBar></WaterProgressBar>
-            <AddWaterBtn></AddWaterBtn>
+            <AddWaterBtn className={style.addBtnStyle1}></AddWaterBtn>
             <img
                 src={bottle1m}
                 srcSet={`
