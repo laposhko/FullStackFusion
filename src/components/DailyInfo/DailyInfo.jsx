4a02f981b69ad786b7cafa22c907ec524fca41f4
RuslanKabachok <ruslan.kabachok@gmail.com>
2024-08-07 17:43:36 +0300

    added AddWaterBtn to DailyInfo

--- src/components/DailyInfo/DailyInfo.jsx
+++ src/components/DailyInfo/DailyInfo.jsx
@@ -3,13 +3,14 @@ import AddWaterBtn from '../AddWaterBtn/AddWaterBtn';
 import WaterList from '../WaterList/WaterList';
 
 import css from '../DailyInfo/DailyInfo.module.css';
+import style from '../AddWaterBtn/AddWaterBtn.module.css';
 
 function DailyInfo() {
   return (
     <div className={css.wrapper}>
       <div className={css.infoWrapper}>
         <ChooseDate />
-        <AddWaterBtn />
+        <AddWaterBtn className={style.addBtnStyle2}></AddWaterBtn>
       </div>
       <WaterList />
     </div>
