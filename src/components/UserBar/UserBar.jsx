c3aed4929ea1dc268bc850a399a01ace0527bf53
laposhko <laposhko0@gmail.com>
2024-08-10 17:22:02 +0300

    fixed some errors in user panel

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -1,9 +1,9 @@
-import { useState, useEffect, useRef } from 'react';
-import { useSelector } from 'react-redux';
-import { HiChevronDown } from 'react-icons/hi';
-import { selectAuthUser } from '../../redux/auth/selectors';
-import css from './UserBar.module.css';
-import UserBarPopover from '../UserBarPopover/UserBarPopover';
+import { useState, useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import { HiChevronDown } from "react-icons/hi";
+import { selectAuthUser } from "../../redux/auth/selectors";
+import css from "./UserBar.module.css";
+import UserBarPopover from "../UserBarPopover/UserBarPopover";
 
 function UserBar() {
   const user = useSelector(selectAuthUser);
@@ -21,27 +21,27 @@ function UserBar() {
   };
 
   const closeOnEscape = (e) => {
-    if (e.key === 'Escape') {
+    if (e.key === "Escape") {
       setIsOpen(false);
     }
   };
 
   useEffect(() => {
-    document.addEventListener('mousedown', closeOnClickOutside);
-    document.addEventListener('keydown', closeOnEscape);
+    document.addEventListener("mousedown", closeOnClickOutside);
+    document.addEventListener("keydown", closeOnEscape);
     return () => {
-      document.removeEventListener('mousedown', closeOnClickOutside);
-      document.removeEventListener('keydown', closeOnEscape);
+      document.removeEventListener("mousedown", closeOnClickOutside);
+      document.removeEventListener("keydown", closeOnEscape);
     };
   }, []);
 
   return (
     <div ref={userBarRef} className={css.userBarWrapper}>
       <button type="button" className={css.userBar} onClick={toggleOpen}>
-        <span>{user.user.name}</span>
-        <img src={user.user.avatar} alt="User avatar" className={css.avatar} />
+        <span>{user.name}</span>
+        <img src={user.avatar} alt="User avatar" className={css.avatar} />
         <HiChevronDown
-          className={`${css.arrowDown} ${isOpen ? css.arrowDownOpen : ''}`}
+          className={`${css.arrowDown} ${isOpen ? css.arrowDownOpen : ""}`}
         />
       </button>
       {isOpen && <UserBarPopover />}
--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,17 +1,16 @@
-import { useSelector } from 'react-redux';
-import css from './UserPanel.module.css';
-import { selectAuthUser } from '../../redux/auth/selectors';
+import { useSelector } from "react-redux";
+import css from "./UserPanel.module.css";
+import { selectAuthUser } from "../../redux/auth/selectors";
 
-import UserBar from '../UserBar/UserBar.jsx';
+import UserBar from "../UserBar/UserBar.jsx";
 
 function UserPanel() {
   const user = useSelector(selectAuthUser);
-
   return (
     <div className={css.userBarWrapper}>
       <h2 className={css.welcome}>
         Hello
-        <span className={css.userName}>,{user.user.name}!</span>
+        <span className={css.userName}>,{user.name}!</span>
       </h2>
       <UserBar />
     </div>
