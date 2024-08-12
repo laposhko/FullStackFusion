3468c344dcefb367f6cab73ce68d7ee51068a2bb
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-08-12 16:38:48 +0300

    upd localhost and npm

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  
   plugins: [react()],
   build: {
     sourcemap: true,
