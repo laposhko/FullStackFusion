86e7d8d687a2331e3bc6345621f9f9c96ce08da1
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-08-13 11:45:45 +0300

    Update MonthInfo.jsx

--- src/components/MonthInfo/MonthInfo.jsx
+++ src/components/MonthInfo/MonthInfo.jsx
@@ -1,58 +1,62 @@
-import Statistics from "../Statistics/Statistics.jsx"
-import Calendar from "../Calendar/Calendar"
+import Statistics from "../Statistics/Statistics.jsx";
+import Calendar from "../Calendar/Calendar";
 import { useState, useMemo } from "react";
 import SvgIcon from "../../img/icons/sprite.jsx";
 import css from "./Monthinfo.module.css";
 import { useSelector } from "react-redux";
 import { selectMonthItems } from "../../redux/water/selectors.js";
 
-
-
 const ToggleComponent = () => {
-    const [isComponentCalendar, setIsComponentCalendar] = useState(true);
-  
-    const handleToggle = () => {
-      setIsComponentCalendar(!isComponentCalendar);
-    };
-  
-    // const monthArray = useSelector(selectWaterAmountForDay);
-    const monthArray = useSelector(selectMonthItems);
-    // const waterState = useSelector(selectWaterState);
+  const [isComponentCalendar, setIsComponentCalendar] = useState(true);
 
-    console.log(monthArray);
-   
+  const handleToggle = () => {
+    setIsComponentCalendar(!isComponentCalendar);
+  };
 
-    const formattedMonthArray = useMemo(() => {
-        return monthArray.map((day) => {
-            const dayPart = day.date.split(' ')[0].split('-')[2];
-            return {
-              id: day.id,
-              date: dayPart,
-              value: Math.floor(Number(day.volume) / 1000),
-            };
-          });
-        }, [monthArray]);
+  // const monthArray = useSelector(selectWaterAmountForDay);
+  // const waterState = useSelector(selectWaterState);
 
-    return (
-      <div>
-        <button onClick={handleToggle}>
-          <SvgIcon
-            className={css.closeIcon}
-            iconName="icon-pie-chart-02"
-            width={24}
-            height={24}
-          />
-        </button>
-  
-        
-        {isComponentCalendar ? (
+  const monthArray = useSelector(selectMonthItems);
+
+  console.log(monthArray);
+
+  const formattedMonthArray = useMemo(() => {
+    const groupedByDate = monthArray.reduce((acc, day) => {
+      const dayPart = day.date.split(" ")[0]; // Отримуємо лише частину з датою "YYYY-MM-DD"
+      if (!acc[dayPart]) {
+        acc[dayPart] = 0;
+      }
+      acc[dayPart] += Number(day.volume);
+      return acc;
+    }, {});
+
+    // Перетворюємо об'єкт у масив, де кожен елемент містить дату та суму об'ємів за цей день
+    return Object.keys(groupedByDate).map((date) => {
+      return {
+        date: date.split("-")[2], // Отримуємо день місяця з дати
+        value: Math.floor(groupedByDate[date]), // Перетворюємо мілілітри на літри, якщо потрібно
+      };
+    });
+  }, [monthArray]);
+
+  return (
+    <div>
+      <button onClick={handleToggle}>
+        <SvgIcon
+          className={css.closeIcon}
+          iconName="icon-pie-chart-02"
+          width={24}
+          height={24}
+        />
+      </button>
+
+      {isComponentCalendar ? (
         <Calendar />
       ) : (
         <Statistics data={formattedMonthArray} />
       )}
+    </div>
+  );
+};
 
-      </div>
-    );
-  };
-  
-  export default ToggleComponent;
+export default ToggleComponent;
