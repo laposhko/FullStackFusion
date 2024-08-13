3c134e715beb41d08304b13471522e48c7f7223a
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-08-13 22:52:34 +0300

    Update CustomTooltip.jsx

--- src/components/Statistics/CustomTooltip.jsx
+++ src/components/Statistics/CustomTooltip.jsx
@@ -1,7 +1,10 @@
 import css from './CustomTooltip.module.css';
 
 const customTooltip = ({ active, payload, coordinate }) => {
-  if (active && payload) {
+  if (active && payload && payload.length) {
+
+    // const data = payload[0]?.payload; 
+
     return (
       <div
         className={css.wrapper}
@@ -9,7 +12,7 @@ const customTooltip = ({ active, payload, coordinate }) => {
           left: `${coordinate.x}px`,
         }}
       >
-        <p style={{ margin: 0, padding: 0 }}>{`${payload[0].value} ml`}</p>
+        <p style={{ margin: 0, padding: 0 }}>{`${payload[0].value ?? 0} ml`}</p>
         <div className={css.triangle}></div>
       </div>
     );
