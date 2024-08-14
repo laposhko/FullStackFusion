cefb63d0e74c84b771dcaae2981fbe64a3f1ef38
laposhko <laposhko0@gmail.com>
2024-08-14 12:28:24 +0300

    moved languages

--- src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -3,13 +3,14 @@ import DailyInfo from "../DailyInfo/DailyInfo";
 import Calendar from "../Calendar/Calendar";
 import css from "./WaterDetailedInfo.module.css";
 // import ToggleComponent from "../MonthInfo/MonthInfo";
-
+import Languages from "../Languages/Languages";
 export default function WaterDetailedInfo() {
   return (
     <div className={css.rightSectionWrapper}>
       <UserPanel />
       <DailyInfo />
       <Calendar />
+      <Languages></Languages>
       {/* <ToggleComponent/> */}
     </div>
   );
