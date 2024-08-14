2ebf09d6886b4683e0c70d7584cce2e8e74e5992
Hatori <ahgangadm@gmail.com>
2024-08-14 15:20:38 +0300

    Replace plain text with a translation function

--- src/components/GoogleAuthorization/GoogleAuthorization.jsx
+++ src/components/GoogleAuthorization/GoogleAuthorization.jsx
@@ -1,12 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
 import { googleAuthLink } from "../../redux/auth/operations.js";
 import { selectAuthGoogleLink } from "../../redux/auth/selectors.js";
+import { useTranslation } from "react-i18next";
 import { useEffect } from "react";
 import { FcGoogle } from "react-icons/fc";
 import css from "./GoogleAuthorization.module.css";
 
 export default function GoogleAuthorization() {
   const dispatch = useDispatch();
+  const { t } = useTranslation();
   const selectorGoogleLink = useSelector(selectAuthGoogleLink);
 
   useEffect(() => {
@@ -15,7 +17,7 @@ export default function GoogleAuthorization() {
   return (
     <a href={selectorGoogleLink} className={css.googleLink}>
       <FcGoogle className={css.googleIcon} />
-      Login with Google
+      {t("GoogleAuthBtn.text")}
     </a>
   );
 }
